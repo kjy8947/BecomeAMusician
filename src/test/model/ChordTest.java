8c285e0f4bf626a5a99366a39b65e24a57d22b70
kjy8947 <b2j9q@ugrad.cs.ubc.ca>
2021-03-04 21:58:14 -0800

    Phase 2 (complete) ver 6 please be final

--- src/test/model/ChordTest.java
+++ src/test/model/ChordTest.java
@@ -7,31 +7,50 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChordTest {
+
     @Test
     public void testDummyChordConstructor() {
         Chord dummyChordForTest = new Chord();
     }
 
     @Test
-    public void testChordConstructor() {
+    public void testChordConstructorMajorChord() {
         Chord chordForTest = new Chord("C");
         assertEquals("C", chordForTest.getRootNote());
     }
 
     @Test
-    public void testBuildMajorTriadChord() {
-        Note note = new Note();
-        Chord chordForTest = new Chord("random string");
-        assertEquals("E", chordForTest.buildMajorTriadChord(4).get(1));
-        assertFalse(chordForTest.buildMajorTriadChord(13).get(0).equals("H"));
+    public void testChordConstructorMinorChord() {
+        Chord chordForTest = new Chord("c");
+        assertEquals("c", chordForTest.getRootNote());
     }
 
     @Test
-    public void testBuildMinorTriadChord() {
-        Note note = new Note();
-        Chord chordForTest = new Chord("random string");
+    public void testBuildMajorTriadChordValid() {
+        Chord chordForTest = new Chord();
+        assertEquals("E", chordForTest.buildMajorTriadChord(4).get(1));
+    }
+
+    @Test
+    public void testBuildMajorTriadChordNotValid() {
+        Chord chordForTest = new Chord();
+        assertEquals(chordForTest.buildMajorTriadChord(Scale.MAX_NOTE_NUMBERS + 1).toString(),
+                "[Invalid input. Please enter a letter between 'A' and 'G#' "
+                        + "(an uppercase letter for a major chord; a lowercase letter for a minor chord).]");
+    }
+
+    @Test
+    public void testBuildMinorTriadChordValid() {
+        Chord chordForTest = new Chord();
         assertEquals("d#", chordForTest.buildMinorTriadChord(4).get(1));
-        assertFalse(chordForTest.buildMinorTriadChord(13).get(0).equals("h"));
+    }
+
+    @Test
+    public void testBuildMinorTriadChordNotValid() {
+        Chord chordForTest = new Chord();
+        assertEquals(chordForTest.buildMinorTriadChord(Scale.MAX_NOTE_NUMBERS + 1).toString(),
+                "[Invalid input. Please enter a letter between 'a' and 'g#' "
+                        + "(an uppercase letter for a major chord; a lowercase letter for a minor chord).]");
     }
 
     @Test
--- src/test/model/ScaleTest.java
+++ src/test/model/ScaleTest.java
@@ -2,29 +2,47 @@ package model;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScaleTest {
+
     @Test
-    public void testScaleConstructor() {
+    public void testScaleConstructorMajorScale() {
         Scale scaleForTest = new Scale("C");
         assertEquals("C", scaleForTest.getRootNote());
     }
 
     @Test
-    public void testBuildMajorTriadChord() {
-        Note note = new Note();
-        Scale scaleForTest = new Scale("random string");
-        assertEquals("D", scaleForTest.buildMajorScale(4).get(1));
-        assertFalse(scaleForTest.buildMajorScale(13).get(0).equals("H"));
+    public void testScaleConstructorMinorScale() {
+        Scale scaleForTest = new Scale("c");
+        assertEquals("c", scaleForTest.getRootNote());
     }
 
     @Test
-    public void testBuildMinorTriadChord() {
-        Note note = new Note();
+    public void testBuildMajorScaleValid() {
+        Scale scaleForTest = new Scale("random string");
+        assertEquals("D", scaleForTest.buildMajorScale(4).get(1));
+    }
+
+    @Test
+    public void testBuildMajorScaleNotValid() {
+        Scale scaleForTest = new Scale("random string");
+        assertEquals(scaleForTest.buildMajorScale(Scale.MAX_NOTE_NUMBERS + 1).toString(),
+                "[Invalid input. Please enter a letter between 'A' and 'G#' "
+                + "(an uppercase letter for a major scale; a lowercase letter for a minor scale).]");
+    }
+
+    @Test
+    public void testBuildMinorScaleValid() {
         Scale scaleForTest = new Scale("random string");
         assertEquals("c", scaleForTest.buildMinorScale(4).get(0));
-        assertFalse(scaleForTest.buildMinorScale(13).get(0).equals("h"));
+    }
+
+    @Test
+    public void testBuildMinorScaleNotValid() {
+        Scale scaleForTest = new Scale("random string");
+        assertEquals(scaleForTest.buildMinorScale(Scale.MAX_NOTE_NUMBERS + 1).toString(),
+                "[Invalid input. Please enter a letter between 'a' and 'g#' "
+                        + "(an uppercase letter for a major scale; a lowercase letter for a minor scale).]");
     }
 }
